0fe71d55067bc1f1f1a0b83ac4d262ce39d8819e
Eric Mertens <emertens@galois.com>
2012-05-16 01:19:40 -0700

    Refactor the Quasiquoter implemenation

--- Spreadsheet/Quote.hs
+++ Spreadsheet/Quote.hs
@@ -1,25 +1,26 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Spreadsheet.Quote (table) where
 
-import Data.Char
-import Data.Time
-import Language.Haskell.TH.Quote
-import Text.Parsec hiding (Column)
+import Data.Char (isAlphaNum)
+import Data.Time (Day, fromGregorian, toGregorian)
 import Language.Haskell.TH
+import Language.Haskell.TH.Quote (QuasiQuoter(..))
+import Text.Parsec hiding (Column)
 
 import Spreadsheet
-import Spreadsheet.Parser
+import Spreadsheet.Parser (spreadsheetParser)
 
 table :: QuasiQuoter
 table = QuasiQuoter
-  { quoteDec  = tableQ
+  { quoteDec  = tableD
   , quoteExp  = \_ -> fail "table: expressions not supported"
   , quotePat  = \_ -> fail "table: patterns not supported"
   , quoteType = \_ -> fail "table: types not supported"
   }
 
-toConName, toFieldName :: String -> Name
-toConName str   = mkName ("Row_" ++ str)
+toTypeName, toConName, toFieldName :: String -> Name
+toTypeName  str = mkName ("Row_" ++ str)
+toConName   str = mkName ("Row_" ++ str)
 toFieldName str = mkName ("col_" ++ map escape str)
   where
   escape ' ' = '_'
@@ -27,48 +28,56 @@ toFieldName str = mkName ("col_" ++ map escape str)
 
 declParser :: Parsec String () (String, Spreadsheet)
 declParser = do
-  spaces
-  name <- many1 (satisfy isAlphaNum)
-  spaces >> char '=' >> spaces
-  ss <- spreadsheetParser
+  name <- spaces >> identifier
+  _    <- spaces >> char '='
+  ss   <-           spreadsheetParser
   return (name, ss)
+  where
+  identifier = many1 (satisfy isAlphaNum) <?> "identifier"
 
-tableQ :: String -> DecsQ
-tableQ str = case parse declParser "quasiquoter" str of
-  Left err -> fail ("table: Parse failed with error:\n" ++ show err)
-  Right (name, Spreadsheet cols rows) -> do
-    (recName, recDec) <- spreadsheetRecordQ name cols
-    let rowsE = map (spreadsheetRowQ recName) rows
-    valDec  <- valD (varP (mkName name)) (normalB (listE rowsE)) []
-    return [recDec, valDec]
+tableD :: String -> DecsQ
+tableD str =
+  case parse declParser "quasiquoter" str of
+    Left err -> fail ("table: Parse failed with error:\n" ++ show err)
+    Right (name, Spreadsheet cols rows) ->
+      sequence
+        [ spreadsheetRecD typeName conName cols
 
-spreadsheetRowQ :: Name -> [CellValue] -> ExpQ
-spreadsheetRowQ conName xs = appsE (conE conName : map toCell xs)
+        , sigD valName        {- :: -} (appT listT (conT typeName))
+        , valD (varP valName) {- =  -} (normalB (rowsE rows)) []
+        ]
+      where
+      valName  = mkName     name
+      typeName = toTypeName name
+      conName  = toConName  name
+      rowsE    = listE . map (spreadsheetRowE conName)
 
-toCell :: CellValue -> ExpQ
-toCell (NumberV x) = litE (rationalL x)
-toCell (StringV x) = [| x |]
-toCell (DateV   x) = dayE x
-toCell EmptyV      = fail "Empty values not supported"
+spreadsheetRowE :: Name -> [CellValue] -> ExpQ
+spreadsheetRowE conName xs = appsE (conE conName : map cellValueE xs)
+
+cellValueE :: CellValue -> ExpQ
+cellValueE (NumberV x) = litE (rationalL x)
+cellValueE (StringV x) = [| x |]
+cellValueE (DateV   x) = dayE x
+cellValueE EmptyV      = fail "Empty values not supported"
 
 dayE :: Day -> ExpQ
 dayE day = [|fromGregorian y m d|]
   where
   (y,m,d) = toGregorian day
 
-spreadsheetRecordQ :: String -> [Column] -> Q (Name, Dec)
-spreadsheetRecordQ nameStr xs = do
-  let name = toConName nameStr
-  let con = recC name (map toField xs)
-  dec <- dataD (cxt []) name [] [con] [''Show,''Read,''Eq]
-  return (name, dec)
+spreadsheetRecD :: Name -> Name -> [Column] -> DecQ
+spreadsheetRecD typeName conName xs =
+  dataD (cxt []) typeName [] [recC conName fields] [''Show,''Read,''Eq]
+  where
+  fields = map toField xs
 
-toField :: Column -> VarStrictTypeQ
-toField col = varStrictType
-                   (toFieldName (columnName col))
-                   (strictType notStrict (toFieldType (columnType col)))
+  toField col =
+    varStrictType
+      (toFieldName (columnName col))
+      (strictType notStrict (cellTypeT (columnType col)))
 
-toFieldType :: CellType -> TypeQ
-toFieldType (NumberT _) = [t|Rational|]
-toFieldType StringT     = [t|String|]
-toFieldType DateT       = [t|Day|]
+cellTypeT :: CellType -> TypeQ
+cellTypeT (NumberT _) = [t|Rational|]
+cellTypeT StringT     = [t|String|]
+cellTypeT DateT       = [t|Day|]
