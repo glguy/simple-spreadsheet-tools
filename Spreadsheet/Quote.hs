b017b7b51bf0189ae642833f9d5dcefc4f80df72
Eric Mertens <emertens@galois.com>
2012-05-16 09:37:30 -0700

    Refactor Spreadsheet.Quote and add comments

--- Spreadsheet/Quote.hs
+++ Spreadsheet/Quote.hs
@@ -13,7 +13,7 @@ import Spreadsheet.Parser (spreadsheetParser)
 
 table :: QuasiQuoter
 table = QuasiQuoter
-  { quoteDec  = tableD
+  { quoteDec  = tableQ
   , quoteExp  = \_ -> fail "table: expressions not supported"
   , quotePat  = \_ -> fail "table: patterns not supported"
   , quoteType = \_ -> fail "table: types not supported"
@@ -36,25 +36,39 @@ declParser = do
   where
   identifier = many1 (satisfy isAlphaNum) <?> "identifier"
 
-tableD :: String -> DecsQ
-tableD str =
+-- 'tableQ' is a quasi-quoter for top-level spreadsheet declarations
+-- using the ASCII art spreadsheet format.
+tableQ :: String -> DecsQ
+tableQ str =
   case parse declParser "[table|...|]" str of
+    Right (name, ss) -> tableD name ss
     Left err -> do
       loc <- location
       fail (show (updateLocation loc err))
-    Right (name, Spreadsheet cols rows) ->
-      sequence
-        [ spreadsheetRecD typeName conName cols
 
-        , sigD valName        {- :: -} (appT listT (conT typeName))
-        , valD (varP valName) {- =  -} (normalB (rowsE rows)) []
-        ]
-      where
-      valName  = mkName     name
-      typeName = toTypeName name
-      conName  = toConName  name
-      rowsE    = listE . map (spreadsheetRowE conName)
+-- 'tableD' creates top-level spreadsheet declarations
+tableD :: String -> Spreadsheet -> DecsQ
+tableD name (Spreadsheet cols rows) =
+  sequence
+    {- data typeName = conName { col_..., ...} -}
+    [ spreadsheetRecD typeName conName cols
 
+    {- valName :: [typeName] -}
+    , sigD valName (appT listT (conT typeName))
+
+    {- valName = rows -}
+    , valD (varP valName) (normalB (rowsE rows)) []
+    ]
+  where
+  valName  = mkName     name
+  typeName = toTypeName name
+  conName  = toConName  name
+  rowsE    = listE . map (spreadsheetRowE conName)
+
+-- | 'updateLocation' combines the error location of a quasi-quoted
+-- string and the error location inside the quasi-quoted string in
+-- order to have an error position relative to the beginning of the
+-- file.
 updateLocation :: Loc -> ParseError -> ParseError
 updateLocation loc err = setErrorPos newPos err
   where
@@ -63,8 +77,6 @@ updateLocation loc err = setErrorPos newPos err
   col'  = sourceColumn pos + snd (loc_start loc) - 1
   newPos | sourceLine pos == 1 = setSourceLine (setSourceColumn pos col') line'
          | otherwise = setSourceLine pos line'
-
-
 
 spreadsheetRowE :: Name -> [CellValue] -> ExpQ
 spreadsheetRowE conName xs = appsE (conE conName : map cellValueE xs)
