7a1c59bf4f58ad1575c7caf70cedfa34e52d2480
Eric Mertens <emertens@galois.com>
2012-05-15 23:47:12 -0700

    Add quasi-quotation support
    This module provides the "table" quasiquoter
    which allows top level table definitions

--- Spreadsheet/Quote.hs
+++ Spreadsheet/Quote.hs
@@ -5,27 +5,28 @@ import Data.Char
 import Data.Time
 import Language.Haskell.TH.Quote
 import Text.Parsec hiding (Column)
-import qualified Language.Haskell.TH as TH
+import Language.Haskell.TH
 
-import Parser
 import Spreadsheet
+import Spreadsheet.Parser
 
 table :: QuasiQuoter
 table = QuasiQuoter
-  { quoteDec = tableQ
-  , quoteExp = \_ -> fail "table: expressions not supported"
-  , quotePat = \_ -> fail "table: patterns not supported"
+  { quoteDec  = tableQ
+  , quoteExp  = \_ -> fail "table: expressions not supported"
+  , quotePat  = \_ -> fail "table: patterns not supported"
   , quoteType = \_ -> fail "table: types not supported"
   }
 
 
-toConName, toFieldName :: String -> TH.Name
-toConName str   = TH.mkName ("Row_" ++ str)
-toFieldName str = TH.mkName ("col_" ++ map escape str)
+toConName, toFieldName :: String -> Name
+toConName str   = mkName ("Row_" ++ str)
+toFieldName str = mkName ("col_" ++ map escape str)
   where
   escape ' ' = '_'
   escape x   = x
 
+declParser :: Parsec String () (String, Spreadsheet)
 declParser = do
   spaces
   name <- many1 (satisfy isAlphaNum)
@@ -33,39 +34,42 @@ declParser = do
   ss <- spreadsheetParser
   return (name, ss)
 
-tableQ :: String -> TH.Q [TH.Dec]
+tableQ :: String -> DecsQ
 tableQ str = case parse declParser "quasiquoter" str of
   Left err -> fail ("table: Parse failed with error: " ++ show err)
   Right (name, Spreadsheet cols rows) -> do
     (recName, recDec) <- spreadsheetRecordQ name cols
     let rowsE = map (spreadsheetRowQ recName) rows
-    valDec  <- TH.valD (TH.varP (TH.mkName name)) (TH.normalB (TH.listE rowsE)) []
+    valDec  <- valD (varP (mkName name)) (normalB (listE rowsE)) []
     return [recDec, valDec]
 
-spreadsheetRowQ conName xs = TH.appsE (TH.conE conName : map toCell xs)
+spreadsheetRowQ :: Name -> [CellValue] -> ExpQ
+spreadsheetRowQ conName xs = appsE (conE conName : map toCell xs)
 
-toCell (NumberV x) = TH.litE (TH.rationalL x)
+toCell :: CellValue -> ExpQ
+toCell (NumberV x) = litE (rationalL x)
 toCell (StringV x) = [| x |]
 toCell (DateV   x) = dayE x
 toCell EmptyV      = fail "Empty values not supported"
 
-dayE :: Day -> TH.ExpQ
+dayE :: Day -> ExpQ
 dayE day = [|fromGregorian y m d|]
   where
   (y,m,d) = toGregorian day
 
-spreadsheetRecordQ :: String -> [Column] -> TH.Q (TH.Name, TH.Dec)
+spreadsheetRecordQ :: String -> [Column] -> Q (Name, Dec)
 spreadsheetRecordQ nameStr xs = do
   let name = toConName nameStr
-  let con = TH.recC name (map toField xs)
-  dec <- TH.dataD (TH.cxt []) name [] [con] [''Show,''Read,''Eq]
+  let con = recC name (map toField xs)
+  dec <- dataD (cxt []) name [] [con] [''Show,''Read,''Eq]
   return (name, dec)
 
-toField col = TH.varStrictType
+toField :: Column -> VarStrictTypeQ
+toField col = varStrictType
                    (toFieldName (columnName col))
-                   (TH.strictType TH.notStrict (toFieldType (columnType col)))
+                   (strictType notStrict (toFieldType (columnType col)))
 
+toFieldType :: CellType -> TypeQ
 toFieldType (NumberT _) = [t|Rational|]
 toFieldType StringT     = [t|String|]
 toFieldType DateT       = [t|Day|]
-toFieldType EmptyT      = error "toFieldType: EmptyT not supported"
